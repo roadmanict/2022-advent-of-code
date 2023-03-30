fa71c1156e5c4f9b6f46b8a06aecdc0f1c714096
Geert Weggemans <geert@pop-os.localdomain>
2023-03-30 19:51:22 +0200

    Initial commit

---
+++ src/day_1.rs
@@ -0,0 +1,100 @@
+use std::{fs::File, io::Read, path::Path, vec};
+
+struct Food {
+    calories: u32,
+}
+
+struct FoodBag {
+    items: Vec<Food>,
+}
+
+impl FoodBag {
+    fn total_calories(&self) -> u32 {
+        let mut total: u32 = 0;
+        for food in self.items.iter() {
+            total = total + food.calories;
+        }
+
+        total
+    }
+}
+
+struct Elf {
+    food_bag: FoodBag,
+}
+
+impl Elf {
+    fn total_calories(&self) -> u32 {
+        self.food_bag.total_calories()
+    }
+}
+
+pub fn find_elf_carrying_most_calories() -> usize {
+    let data_file_path = Path::new("resources/day_1.txt");
+    let mut data_file = match File::open(data_file_path) {
+        Err(_) => panic!("Error opening file"),
+        Ok(file) => file,
+    };
+    let mut content = String::new();
+    match data_file.read_to_string(&mut content) {
+        Err(_) => panic!("Error reading file"),
+        Ok(_) => {}
+    }
+    let splitted_string = content.split("\\d");
+    for part in splitted_string {
+        if part == "" {
+            println!("Empty line");
+
+            continue
+        }
+
+        print!("{}", part);
+    }
+    let elfs: Vec<Elf> = vec![
+        Elf {
+            food_bag: FoodBag {
+                items: vec![
+                    Food { calories: 1000 },
+                    Food { calories: 2000 },
+                    Food { calories: 3000 },
+                ],
+            },
+        },
+        Elf {
+            food_bag: FoodBag {
+                items: vec![Food { calories: 4000 }],
+            },
+        },
+        Elf {
+            food_bag: FoodBag {
+                items: vec![Food { calories: 5000 }, Food { calories: 6000 }],
+            },
+        },
+        Elf {
+            food_bag: FoodBag {
+                items: vec![
+                    Food { calories: 7000 },
+                    Food { calories: 8000 },
+                    Food { calories: 9000 },
+                ],
+            },
+        },
+        Elf {
+            food_bag: FoodBag {
+                items: vec![Food { calories: 10000 }],
+            },
+        },
+    ];
+
+    let mut elf_with_most_calories = 0;
+    let mut most_calories = 0;
+
+    for (index, elf) in elfs.iter().enumerate() {
+        if elf.total_calories() > most_calories {
+            elf_with_most_calories = index;
+            most_calories = elf.total_calories();
+        }
+    }
+
+    elf_with_most_calories
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use crate::day_1::find_elf_carrying_most_calories;
+
+pub mod day_1;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Day 1 solution: {}", find_elf_carrying_most_calories());
+}
