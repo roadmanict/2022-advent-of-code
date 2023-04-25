7eb82efc4efe5ec0ee3bd568ec18293e2a6eb2b6
Geert Weggemans <geert@Geerts-MacBook-Pro.local>
2023-04-25 14:55:10 +0200

    Implement from_str on stack

--- src/model/stack.rs
+++ src/model/stack.rs
@@ -1,15 +1,32 @@
 use std::str::FromStr;
 
 #[derive(Debug)]
-struct Stack {
-    crates: Vec<char>,
-}
+struct Crate(char);
 
-impl FromStr for Stack {
+impl FromStr for Crate {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        let s = s.trim();
+        let mut iterator = s.chars();
+        iterator.next();
+
+        Ok(Crate(iterator.next().ok_or("Invalid crate")?))
+    }
+}
+
+#[derive(Debug)]
+struct Stack {
+    crates: Vec<Crate>,
+}
+
+impl Stack {
+    fn new() -> Self {
+        Self { crates: vec![] }
+    }
+
+    fn add_crate(&mut self, crte: Crate) {
+        self.crates.push(crte);
     }
 }
 
@@ -22,14 +39,32 @@ impl FromStr for Supplies {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let test = s.lines().map(|line| {
-            let chunks = line.len() / 4;
-            for chunk in 0..chunks {
-                let result = line.split_at(chunk * 4);
-            }
-        }).collect::<Vec<_>>();
+        let mut stacks: Vec<Stack> = vec![];
 
-        todo!()
+        let lines = s.lines();
+        for (index, line) in lines.enumerate() {
+            if index == 0 {
+                let len_stacks = line.len() / 4;
+                for _ in 0..len_stacks {
+                    stacks.push(Stack::new())
+                }
+            }
+
+            let stack = stacks.get_mut(index).ok_or("Stack not available")?;
+
+            let mut line = line;
+            while line.len() > 0 {
+                let (raw_supply, rest) = line.split_at(4);
+                let crte = Crate::from_str(raw_supply);
+                if let Ok(crte) = crte {
+                    stack.add_crate(crte);
+                }
+
+                line = rest;
+            }
+        }
+
+        Ok(Supplies { stacks })
     }
 }
 
@@ -38,13 +73,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_from_str() {
+    fn test_crate_from_str() {
+        let crte = Crate::from_str("[E] ").expect("Crate string could be parsed");
+
+        assert_eq!(crte.0, 'E')
+    }
+
+    #[test]
+    fn test_crate_from_str_invalid_input() {
+        let error = Crate::from_str("    ").err().expect("from_str to return error");
+
+        assert_eq!(error, "Invalid crate");
+    }
+
+    #[test]
+    fn test_supplies_from_str() {
         let supplies = Supplies::from_str(
             "    [D]    
 [N] [C]    
 [Z] [M] [P]
  1   2   3 ",
-        ).unwrap();
+        )
+        .unwrap();
 
         assert_eq!(supplies.stacks.len(), 3)
     }
