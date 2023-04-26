3dd0ecd126993f82c1dd39e6b2d73d55ce5b88ce
Geert Weggemans <geert@Geerts-MacBook-Pro.local>
2023-04-26 14:07:28 +0200

    Finish day 5 part 1

--- src/model/stack.rs
+++ src/model/stack.rs
@@ -1,22 +1,53 @@
-use std::str::FromStr;
+use std::{num::ParseIntError, str::FromStr};
 
 #[derive(Debug)]
-struct Crate(char);
+pub struct MoveCrate {
+    amount: usize,
+    from: usize,
+    to: usize,
+}
 
-impl FromStr for Crate {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.trim();
-        let mut iterator = s.chars();
-        iterator.next();
-
-        Ok(Crate(iterator.next().ok_or("Invalid crate")?))
+impl MoveCrate {
+    fn new(amount: usize, from: usize, to: usize) -> Self {
+        Self { amount, from, to }
     }
 }
 
 #[derive(Debug)]
-struct Stack {
+pub enum MoveCrateFromStrError {
+    Generic(String),
+    ParseIntError(ParseIntError),
+}
+
+impl From<String> for MoveCrateFromStrError {
+    fn from(value: String) -> Self {
+        MoveCrateFromStrError::Generic(value)
+    }
+}
+impl From<ParseIntError> for MoveCrateFromStrError {
+    fn from(value: ParseIntError) -> Self {
+        MoveCrateFromStrError::ParseIntError(value)
+    }
+}
+
+impl FromStr for MoveCrate {
+    type Err = MoveCrateFromStrError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split_string = s.split_whitespace().collect::<Vec<_>>();
+        let amount = split_string[1].parse::<usize>()?;
+        let from = split_string[3].parse::<usize>()?;
+        let to = split_string[5].parse::<usize>()?;
+
+        Ok(MoveCrate { amount, from, to })
+    }
+}
+
+#[derive(Debug)]
+struct Crate(char);
+
+#[derive(Debug)]
+pub struct Stack {
     crates: Vec<Crate>,
 }
 
@@ -28,20 +59,31 @@ impl Stack {
     fn add_crate(&mut self, crte: Crate) {
         self.crates.push(crte);
     }
-}
 
-fn batch_string(s: &str, size: usize) -> &[&str] {
-    let length = s.len();
-    let mut start = 0;
-    while start < length {
-        s.split_at(size);
+    fn remove_crates(&mut self, amount: usize) -> Vec<Crate> {
+        let range = self.crates.len() - amount..self.crates.len();
+
+        self.crates.drain(range).rev().collect::<Vec<_>>()
     }
-    todo!()
 }
 
 #[derive(Debug)]
-struct Supplies {
+pub struct Supplies {
     pub stacks: Vec<Stack>,
+}
+
+impl Supplies {
+    pub fn move_crate(&mut self, move_crate: MoveCrate) {
+        let crates_to_move = self.stacks[move_crate.from - 1].remove_crates(move_crate.amount);
+
+        for ele in crates_to_move {
+            self.stacks[move_crate.to - 1].add_crate(ele);
+        }
+    }
+
+    pub fn get_top_crates(&self) -> String {
+        self.stacks.iter().map(|stack| stack.crates[stack.crates.len() - 1].0.to_string()).collect::<Vec<_>>().concat::<_>()
+    }
 }
 
 impl FromStr for Supplies {
@@ -61,14 +103,11 @@ impl FromStr for Supplies {
 
                     crate_indexes.push(index);
                     stacks.push(Stack::new());
-
-                    println!("index {}, part {}", index, ch);
                 }
 
                 continue;
             }
 
-            println!("indexes: {:?}", crate_indexes);
             let chars = line.chars().collect::<Vec<_>>();
 
             for (index, crate_index) in crate_indexes.iter().enumerate() {
@@ -90,21 +129,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_crate_from_str() {
-        assert_eq!("    ".split_at(4), ("    ", ""));
-        let crte = Crate::from_str("[E] ").expect("Crate string could be parsed");
-
-        assert_eq!(crte.0, 'E')
-    }
-
-    #[test]
-    fn test_crate_from_str_invalid_input() {
-        let error = Crate::from_str("    ").expect_err("from_str to return error");
-
-        assert_eq!(error, "Invalid crate");
-    }
-
-    #[test]
     fn test_supplies_from_str() {
         let supplies = Supplies::from_str(
             "    [D]    
@@ -113,7 +137,6 @@ mod tests {
  1   2   3 ",
         )
         .expect("Expect Supplies string to be parsed");
-        println!("{:?}", supplies);
 
         assert_eq!(supplies.stacks.len(), 3);
         assert_eq!(supplies.stacks[0].crates[0].0, 'Z');
@@ -122,5 +145,31 @@ mod tests {
         assert_eq!(supplies.stacks[1].crates[1].0, 'C');
         assert_eq!(supplies.stacks[1].crates[2].0, 'D');
         assert_eq!(supplies.stacks[2].crates[0].0, 'P');
+    }
+
+    fn test_move_crate_from_str() {
+        let move_crate = MoveCrate::from_str("move 1 from 2 to 1").unwrap();
+
+        assert_eq!(move_crate.amount, 1);
+        assert_eq!(move_crate.from, 2);
+        assert_eq!(move_crate.to, 2);
+    }
+
+    fn test_remove_from_crate() {
+        let mut stack = Stack::new();
+        stack.add_crate(Crate('S'));
+        stack.add_crate(Crate('A'));
+        stack.add_crate(Crate('D'));
+        stack.add_crate(Crate('F'));
+
+        let removed_crates = stack.remove_crates(2);
+
+        assert_eq!(removed_crates.len(), 2);
+        assert_eq!(removed_crates[0].0, Crate('F').0);
+        assert_eq!(removed_crates[1].0, Crate('D').0);
+
+        assert_eq!(stack.crates.len(), 2);
+        assert_eq!(stack.crates[0].0, Crate('S').0);
+        assert_eq!(stack.crates[1].0, Crate('A').0);
     }
 }
--- tests/2022_day_5.rs
+++ tests/2022_day_5.rs
@@ -1,2 +1,20 @@
+use std::str::FromStr;
+
+use advent::{utils::file_reader::FileReader, model::stack::{Supplies, MoveCrate}};
+
 #[test]
-fn test_day_5() {}
+fn test_day_5() {
+    let file_reader: FileReader = FileReader::new();
+    let input = file_reader.read_file("resources/day_5.txt").unwrap();
+
+    let (supplies, moves) = input.split_once("\n\n").expect("Cannot split by double newline");
+    let mut supplies = Supplies::from_str(supplies).expect("Cannot parse supplies");
+
+    let moves = moves.lines().map(|line| MoveCrate::from_str(line).expect("Cannot parse movecrate"));
+
+    for move_crate in moves {
+        supplies.move_crate(move_crate);
+    }
+
+    assert_eq!(supplies.get_top_crates(), "");
+}
